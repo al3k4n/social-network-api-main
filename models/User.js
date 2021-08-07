3d7163412779c3c32a966f1d01e28ecf0c0e6e0c
al3k4n <63288974+al3k4n@users.noreply.github.com>
2021-08-07 15:05:44 +0400

    Revert "Revert "Initial commit""
    This reverts commit dece17b057dd4e4af28b9e79aa4c9e831ae8184e.

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// получаем друзей
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// строим схему между ними
+const User = model("User", userSchema);
+
+// TODO разобраться с посмтроением модели
+module.exports = User;
